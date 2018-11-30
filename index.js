692bce6f8c90c9282b87cd9a37d9d20327dbcb4b
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-30 05:45:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+let recipes = { enchiladas: 2 };
+
+let updateObjectWithKeyAndValue = (object, key, value) => {
+  return Object.assign(recipes, { [poppyseed cake]: 1 })
+  // return Object.assign(playlist, { [artistName]: songTitle })
+};
